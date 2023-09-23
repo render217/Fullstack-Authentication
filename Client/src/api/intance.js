03de8f5002fa74d1ab7e0695837ce4ef8ad70e52
render217 <render21712@gmail.com>
2023-09-23 22:05:25 +0300

    updated BASE_URL

--- Client/src/api/intance.js
+++ Client/src/api/intance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = import.meta.env.VITE_SERVER_URI;
-const BASE_URL = "https://auth-backend-eh7f.onrender.com";
+const BASE_URL = "https://fair-erin-gecko-cape.cyclic.app";
 
 export const httpClient = axios.create({
   baseURL: BASE_URL,
