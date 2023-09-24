03944293125b9694c45dbd810ce7657ab4aa4297
render217 <render21712@gmail.com>
2023-09-24 14:19:38 +0300

    ...

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -6,25 +6,20 @@ router.post("/login", authController.login);
 router.post("/register", authController.register);
 router.get("/logout", authController.logout);
 router.get("/status", authController.checkLoginStatus);
-router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
-router.get(
-  "/google/callback",
-  passport.authenticate("google"),
-  authController.socialLogin
-);
+router
+  .route("/google")
+  .get(passport.authenticate("google", { scope: ["email", "profile"] }));
+  
+router
+  .route("/google/callback")
+  .get(passport.authenticate("google"), authController.socialLogin);
 
-router.get(
-  "/github",
-  passport.authenticate("github", { scope: ["profile", "email"] })
-);
+router
+  .route("/github")
+  .get(passport.authenticate("github", { scope: ["profile", "email"] }));
 
-router.get(
-  "/github/callback",
-  passport.authenticate("github"),
-  authController.socialLogin
-);
+router
+  .route("/github/callback")
+  .get(passport.authenticate("github"), authController.socialLogin);
 
 module.exports = router;
