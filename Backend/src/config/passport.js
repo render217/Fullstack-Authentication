f11ac5b3191b19382f984839f7fefa0bff9e2c52
render217 <render21712@gmail.com>
2023-09-24 13:54:29 +0300

    passport config

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -12,8 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      callbackURL: "https://fullstack-auth.onrender.com/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ email: profile._json.email });
