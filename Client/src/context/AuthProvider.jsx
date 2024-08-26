8a8d2849318c0f6e590554b7aa2860dda1c8b147
render <142716427+render217@users.noreply.github.com>
2024-08-26 15:01:24 +0300

    loading fixed

--- Client/src/context/AuthProvider.jsx
+++ Client/src/context/AuthProvider.jsx
@@ -1,41 +1,43 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import useProfile from "../hooks/useProfile";
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createContext, useContext, useEffect, useState } from "react";
+// import useProfile from "../hooks/useProfile";
 import { checkLogInStatus } from "../api";
 import { Loader } from "../components";
-import { requestHandler } from "../util";
+// import { requestHandler } from "../util";
 
 const AuthContext = createContext({
-  isAuth: {},
-  setIsAuth: () => {},
-  getLoggedIn: async () => {},
+    isAuth: {},
+    setIsAuth: () => {},
+    getLoggedIn: async () => {},
 });
 const AuthProvider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(undefined);
+    const [isAuth, setIsAuth] = useState(undefined);
 
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
-  const getLoggedIn = async () => {
-    const { data } = await checkLogInStatus();
-    setIsAuth(data);
-  };
+    const getLoggedIn = async () => {
+        const { data } = await checkLogInStatus();
+        setIsAuth(data);
+    };
 
-  async function _initialize() {
-    setIsLoading(true);
-    setTimeout(async () => {
-      await getLoggedIn();
-      setIsLoading(false);
-    }, 1000);
-  }
+    useEffect(() => {
+        async function _initialize() {
+            setIsLoading(true);
+            // setTimeout(async () => {
+            await getLoggedIn();
+            setIsLoading(false);
+            // }, 1000);
+        }
 
-  useEffect(() => {
-    _initialize();
-  }, []);
+        _initialize();
+    }, []);
 
-  return (
-    <AuthContext.Provider value={{ isAuth, getLoggedIn, setIsAuth }}>
-      {isLoading ? <Loader /> : children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ isAuth, getLoggedIn, setIsAuth }}>
+            {isLoading ? <Loader /> : children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
